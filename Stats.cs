511aa2dd5327e22d6879119cb3e0b2ab2d5a8ffd
CoMFA_JNSP <63331865+KalebDHedges@users.noreply.github.com>
2023-05-30 00:38:00 +1000

    Added properties to Stats, as well as random generation of those properties on construction.

--- Stats.cs
+++ Stats.cs
@@ -1,6 +1,30 @@
-﻿namespace Cyberpunk2020_CharacterGenerator
+﻿using System.Runtime.InteropServices;
+
+namespace Cyberpunk2020_CharacterGenerator
 {
     internal class Stats
     {
+        public Stats()
+        {
+            Intelligence    = (byte)Die.RollDie(3, 10);
+            Reflexes        = (byte)Die.RollDie(3, 10);
+            Cool            = (byte)Die.RollDie(3, 10);
+            Technical       = (byte)Die.RollDie(3, 10);
+            Luck            = (byte)Die.RollDie(3, 10);
+            Attractiveness  = (byte)Die.RollDie(3, 10);
+            Movement        = (byte)Die.RollDie(3, 10);
+            Empathy         = (byte)Die.RollDie(3, 10);
+            Body            = (byte)Die.RollDie(3, 10);
+        }
+
+        public byte Intelligence { get; init; }
+        public byte Cool { get; init; }
+        public byte Reflexes { get; init; }
+        public byte Attractiveness { get; init; }
+        public byte Technical { get; init; }
+        public byte Luck { get; init; }
+        public byte Body { get; init; }
+        public byte Empathy { get; init; }
+        public byte Movement { get; init; }
     }
 }
